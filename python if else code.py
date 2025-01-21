44105ad6743cd588db26c209cd6953b15caf1e54
Swaroopa006 <swaroopadas2006@gmail.com>
2025-01-21 17:28:29 +0530

    Add files via upload

---
+++ python if else code.py
@@ -0,0 +1,66 @@
+"""def Ls3():
+    a = int(input("Enter the value of a: "))
+    b = int(input("Enter the value of b: "))
+    c = int(input("Enter the value of c: "))
+    if(a>b) and (a>c):
+        print("The largest value is ", a)
+    elif(b>c):
+        print("The largest value is ", b)
+    elif(c>b):
+        print("The largest value is", c)
+    else:
+        print("All numbers are equal")
+Ls3()
+Ls3()
+Ls3()"""
+#This will call the code 3 times
+"""def Num1():
+    n1 = int(input("Enter the value of n1: "))
+    if(n1%2 == 0):
+        print("Even number is n1 = ", n1)
+    else:
+        print("Odd number is n1 = ", n1)
+Num1()
+Num1()"""
+
+"""def A1():
+    n1 = int(input("Enter the value of n1: "))
+    if(n1%10 == 0):
+        print("n1 is Divisible by 10")
+    else:
+        print("n1 is not Divisible by 10")
+A1()
+
+def Ag():
+    Age = int(input("The age of a person is:"))
+    if(Age < 18):
+        print("Minor")
+    else:
+        print("Major")
+Ag()
+
+def N2():
+    n1 = int(input("Enter the value of n1: "))
+    n2 = str(n1)
+    n3 = len(n2)
+    print("The number of digits in a number :", n3)
+N2()
+
+def Y1():
+    Year = int(input("The Year is:"))
+    if(Year%4 == 0):
+        print("Leap year")
+    else:
+        print("Not Leap year")
+Y1()"""
+
+def An1():
+    a1 = int(input("Enter the value of angle 1: "))
+    a2 = int(input("Enter the value of angle 2: "))
+    a3 = int(input("Enter the value of angle 3: "))
+    if(a1+a2+a3 == 180):
+        print("Valid triangle")
+    else:
+        print("Not valid")
+An1()
+
