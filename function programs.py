0b6c050077c8c43684d2c8b76cc68d33c5a14303
Swaroopa006 <swaroopadas2006@gmail.com>
2025-04-23 00:40:20 +0530

    Update and rename functio python.py to function programs.py

--- function programs.py
+++ function programs.py
@@ -1,4 +1,5 @@
-'''def count_lower_upper(str):
+#q1)
+def count_lower_upper(str):
     s= 0
     for i in str:
         if (i.isupper()):
@@ -15,7 +16,8 @@ def input_():
     print(d)
 input_()
 
-q2)def addval(n):
+#q2)
+def addval(n):
     s = 0
     v = n
     for i in range(4):
@@ -27,7 +29,8 @@ q2)def addval(n):
 for x in range(4,8):
     print(addval(x))
 
-q3) def create_array1(x,n):
+#q3) 
+    def create_array1(x,n):
     I =[]
     for i in range(x):
         I = I +[n]
@@ -102,8 +105,68 @@ def tuple_in_list(x):
         l.append(k)
     return l
 x = int(input("Enter a number:"))
-print(tuple_in_list(x))'''
+print(tuple_in_list(x))
 
+#q7)
+def ispalindrome(x):
+    if x==x[::-1]:
+        return (True)
+    else:
+        return(False)
+x=input("enter the string:")
+print(ispalindrome(x))
+
+#q8)
+def convert(s1):
+    s=set(s1)
+    l=str(s)
+    print(l)
+    s1="".join(sorted(s))
+    return s1
+s1=input("enter a string:")
+print(convert(s1))
+
+#q9)
+def count_alpha_digits(s):
+    l=list(s)
+    a=0
+    n=0
+    for i in l:
+        if i.isalpha():
+            a+=1
+        elif i.isdigit():
+            n+=1
+    return(a,n)
+
+s=input("enter a string:")
+a,n=count_alpha_digits(s)
+d={'alpha':a,'number':n}
+print(d)
+
+#10)
+def frequency(s):
+    word=s.split()
+    l={}
+    for i in word:
+        i=i.lower()
+        if i in l:
+            l[i]+=1
+        else:
+            l[i]=1
+    f=sorted(l.items())
+    return dict(f)
+s=input("enter a string:")
+r=frequency(s)
+print(r)
+
+#11)
+def create_list(l1,l2):
+     inter = list(set(l1)&set(l2))
+    return inter
+l1=[1,2,3,4,5]
+l2=[2,5,6,7,8]
+r=create(l1,l2)
+print(r)
 
     
     
